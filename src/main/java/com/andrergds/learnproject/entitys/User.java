f1bd2ca854d0742f9204c10f88e1e65b9a2f48f4
Andrezitos <tapicon58@gmail.com>
2026-03-10 23:49:01 -0300

    Project Created

---
+++ src/main/java/com/andrergds/learnproject/entitys/User.java
@@ -0,0 +1,82 @@
+package com.andrergds.learnproject.entitys;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+    public static final long serialVersonUID = 1L;
+    //variables private String name;
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+
+    //constructor / obrigatorio constructor vazio por estar utilzando framework
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    public User(){
+
+    }
+
+    public User(Long id, String name, String email, String phone, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
